cce1f47fda90b9045c2d10b89bfac71d6ab01405
4PHRODIT3 <b8_htetmyatlinn@uit.edu.mm>
2021-12-19 19:44:13 +0630

    Added More Content and update Design

---
+++ assets/js/carousel.js
@@ -0,0 +1,34 @@
+$(".portfolio").slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
--- assets/js/slidebar.js
+++ assets/js/slidebar.js
@@ -1,9 +1,38 @@
 var sidebar_off_btn = document.getElementById("close-sidebar");
 var sidebar_in_btn = document.getElementById("open-sidebar");
 var sidebar = document.getElementById("sidebar");
+var nav_tabs = document.querySelector(".nav-tabs");
+
 const sidebar_toggler = (_) => {
-  sidebar.classList.toggle("nav-slide-off");
+  let screen_width = screen.width;
+  if (screen_width <= 1050) {
+    sidebar.classList.toggle("nav-slide-off");
+    document.body.classList.toggle("overflow-y-scroll");
+  }
+};
+
+const setActive = (id) => {
+  $(".nav-tabs a[href]").removeClass("active");
+  $(`.nav-tabs a[href="#${id}"`).addClass("active");
 };
 
 sidebar_off_btn.addEventListener("click", sidebar_toggler);
 sidebar_in_btn.addEventListener("click", sidebar_toggler);
+nav_tabs.addEventListener("click", sidebar_toggler);
+
+const setUpNavActions = (_) => {
+  $("section[id]").waypoint(
+    function (direction) {
+      let current_id = $(this.element).attr("id");
+      if (direction == "down") {
+        setActive(current_id);
+      } else {
+        setActive(current_id);
+      }
+    },
+    {
+      offset: 0,
+    }
+  );
+};
+setUpNavActions();
