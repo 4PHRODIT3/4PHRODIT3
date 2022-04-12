67abb103b04ff25007f64ad3b3d356d30f55bd20
Htet-Myat-Linn <b8_htetmyatlinn@uit.edu.mm>
2022-04-13 02:17:46 +0630

    Performance Optimize

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,10 @@ var nav_tabs = document.querySelector(".nav-tabs");
 
 const sidebar_toggler = (_) => {
   let screen_width = screen.width;
-  sidebar.classList.toggle("nav-slide-off");
-  document.body.classList.toggle("overflow-y-scroll");
+  if (screen_width < 1366) {
+    sidebar.classList.toggle("nav-slide-off");
+    document.body.classList.toggle("overflow-y-scroll");
+  }
 };
 
 const setActive = (id) => {
@@ -37,46 +39,71 @@ setUpNavActions();
 
 window.onload = (_) => {
   document.querySelector(".loader-container").remove();
+  document.querySelector(".main-container").style.display = "block";
+  $(".portfolio").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1920,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 1380,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
 };
 
-$(".portfolio").slick({
-  dots: true,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1920,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 1380,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 800,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      },
-    },
-  ],
-});
+jQuery.event.special.touchstart = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("touchstart", handle, {
+      passive: !ns.includes("noPreventDefault"),
+    });
+  },
+};
+jQuery.event.special.touchmove = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("touchmove", handle, {
+      passive: !ns.includes("noPreventDefault"),
+    });
+  },
+};
+jQuery.event.special.wheel = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("wheel", handle, { passive: true });
+  },
+};
+jQuery.event.special.mousewheel = {
+  setup: function (_, ns, handle) {
+    this.addEventListener("mousewheel", handle, { passive: true });
+  },
+};
